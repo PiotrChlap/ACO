f3e4a191cbc0d857c893508926e44c1dcfc47b3e
Piotr Chłapiński <p.chlapinski@gmail.com>
2021-12-16 11:04:50 +0100

    PiotrChlap
    Add some func

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
+import numpy as np
+import random as rand
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+places = []
+tracePheromone = []
+cologne = []
+bestAnt = None
+
+class ant:
+    placesVisited = []
+
+    def __init__(self,place):
+        self.placesVisited.append(place)
+
+    def vistPlace(self, place):
+        self.placesVisited.append(place)
+
+    def getDistance(self):
+        distance = 0
+        for x in range(1, len(self.placesVisited)):
+            distance += np.linalg.norm(self.placesVisited[x][1] - self.placesVisited[x - 1][1])
+        return distance
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def createColegne(size):
+    for x in range(size):
+        startPlace = rand.randint(1, int(len(places)))
+        cologne.append(ant(places[startPlace-1]))
 
 
-# Press the green button in the gutter to run the script.
+def getBestTrace():
+    if(bestAnt == None):
+        tmpBestAnt = cologne[0]
+    else:
+        tmpBestAnt = bestAnt
+    for x in range(1, len(cologne)):
+        if(tmpBestAnt.getDistance() > cologne[x]):
+            tmpBestAnt = cologne[x]
+    global bestAnt
+    bestAnt = tmpBestAnt
+
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    files = open("A-n32-k5.txt", "r")
+    for x in files:
+        tmp = x.split()
+        name =[]
+        name.append(tmp[0])
+        tmp.remove(tmp[0])
+        tmp = [int(i) for i in tmp]
+        tmp = np.array(tmp)
+        name.append(tmp)
+        places.append(name)
+    print(places)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for x in range(len(places)):
+        tracePheromone.append([1]*len(places))
+    print(tracePheromone)
+
+
+
+
+
